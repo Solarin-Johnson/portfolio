eabd6a759862fc478f156643de7736f06bfdde8c
Solarin <solarinjohnson@gmail.com>
2023-10-14 12:59:49 +0100

    ___

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -2,7 +2,7 @@ import './info.scss'
 function Info(){
     return(
     <div className="info">
-        <div id="name">Hi, I'm Solarin Johnson, a <br/> <strong>Web Designer</strong></div>
+        <div id="name">Hi, I'm Solarin Johnson, a <strong>Web Designer</strong></div>
         <div id="description"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat officia voluptatum nesciunt earum doloribus ut et, magni, quasi totam cum impedit facere incidunt consectetur qui.</div>
         <div id="button"></div>
     </div>
