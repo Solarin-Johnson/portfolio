1210137abaed15068a1ecd6bed3697c70cb9f651
Solarin <solarinjohnson@gmail.com>
2023-10-16 00:20:54 +0100

    ...

--- src/components/contacts/Contacts.jsx
+++ src/components/contacts/Contacts.jsx
@@ -1,10 +1,19 @@
 import './contacts.scss'
 function Contact(){
+    const fb = () => {
+        window.open('https://www.facebook.com/johnson.solarin/')
+    }
+    const twitter = () => {
+        window.open('https://twitter.com/S0LARIN')
+    }
+    const git = () => {
+        window.open('https://www.github.com/solarin-johnson')
+    }
     return(
         <div className="contacts">
-            <span className="fab fa-facebook"></span>
-            <span className="fab fa-twitter"></span>
-            <span className="fab fa-github"></span>
+            <span onClick={fb} className="fab fa-facebook"></span>
+            <span onClick={git} className="fab fa-github"></span>
+            <span onClick={twitter} className="fab fa-twitter"></span>
         </div>
     )
 }
