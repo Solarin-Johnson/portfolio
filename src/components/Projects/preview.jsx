0d82ae8f630320a421e4404be48581cbae0a82e5
Solarin <solarinjohnson@gmail.com>
2023-10-17 05:25:00 +0100

    sleep man

--- src/components/Projects/preview.jsx
+++ src/components/Projects/preview.jsx
@@ -3,13 +3,16 @@ function Preview(props){
     const screenshot = {
         backgroundImage: `url(${props.screenshot})`
     }
+    const explore = () =>{
+        window.open(props.link)
+    }
     return(
         <div className="preview">
            <div id="preview_text">
                 <div id="project_name">{props.name}</div>
                 <div id="project_description">{props.description}</div>
                 <div id="languages">{props.languages}</div>
-                <div id="explore">Explore {props.name}</div>
+                <div id="explore" onClick={explore}>Explore {props.name}</div>
            </div>
            <div id="preview_image" style={screenshot}>
                 
