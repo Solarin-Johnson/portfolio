137740839da461cd6a9d48939e90c03086d5a5be
Solarin <solarinjohnson@gmail.com>
2023-10-17 02:37:33 +0100

    nth-child .previw

--- src/components/Projects/preview.jsx
+++ src/components/Projects/preview.jsx
@@ -3,7 +3,7 @@ function Preview(props){
     return(
         <div className="preview">
            <div id="preview_text">
-                
+                <div id="project_name">{props.name}</div>
            </div>
            <div id="preview_image">
             
