bae3bb50b59d256cd891b049ada273564b7a35c5
Solarin <solarinjohnson@gmail.com>
2023-10-17 02:05:34 +0100

    menu backdrop

--- src/components/Projects/preview.jsx
+++ src/components/Projects/preview.jsx
@@ -2,7 +2,12 @@
 function Preview(props){
     return(
         <div className="preview">
-           
+           <div id="preview_text">
+                
+           </div>
+           <div id="preview_image">
+            
+           </div>
         </div>
     )
 }
