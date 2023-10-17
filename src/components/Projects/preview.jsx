be4d385a9b1a89e7d6da7ae5c85c4e28097fb374
Solarin <solarinjohnson@gmail.com>
2023-10-17 20:10:05 +0100

    projects done

--- src/components/Projects/preview.jsx
+++ src/components/Projects/preview.jsx
@@ -6,13 +6,21 @@ function Preview(props){
     const explore = () =>{
         window.open(props.link)
     }
+    const git = () =>{
+        window.open(props.git_repo)
+    }
     return(
         <div className="preview">
            <div id="preview_text">
                 <div id="project_name">{props.name}</div>
                 <div id="project_description">{props.description}</div>
                 <div id="languages">{props.languages}</div>
-                <div id="explore" onClick={explore}>Explore {props.name}</div>
+                <div id="last">
+                    <div id="explore" onClick={explore}>Explore {props.name}</div>
+                    <div id="github_repo">
+                            <span onClick={git} className="fab fa-github" title="View Github Repository"></span>
+                    </div>
+                </div>
            </div>
            <div id="preview_image" style={screenshot}>
                 
